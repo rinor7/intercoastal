1e00f9cba9a0ecd54a00c1b2d14c0fbc1844bd7b
Rinor <rinorsahiti@hennessey.com>
2026-01-29 23:53:03 +0100

    add changes

---
+++ template-simple-content.php
@@ -0,0 +1,14 @@
+<?php
+/* Template Name: Simple Content */
+get_header();
+?>
+
+<main id="primary" class="site-main site-simple-content">
+
+<div class="container">
+    <?php the_content(); ?>
+</div>
+
+</main>
+
+<?php get_footer(); ?>
