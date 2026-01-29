70045474656afc628b2ac324f1ce780ecd0937ad
Rinor <rinorsahiti@hennessey.com>
2026-01-29 23:52:04 +0100

    add changes

--- page.php
+++ page.php
@@ -2,11 +2,13 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-default site-page">
+	<main id="primary" class="site-default site-page-default">
 
-		<div class="container">
-			<?php the_content(); ?>
-		</div>
+		<?php get_template_part('includes/blocks/block-banner-all', null, array()); ?>
+
+		<?php get_template_part('includes/blocks/block-image-and-content-comb', null, array()); ?>
+
+		<?php get_template_part('includes/blocks/block-features-four', null, array()); ?>
 
 	</main><!-- #main -->
 
